09ac49a5446cfffaa40a164d71a6a180358c8e8a
Rob Duquette <132164984+robqduquette@users.noreply.github.com>
2024-04-29 14:56:41 -0400

    Added LQR Bode plots.

--- src/LqrTrans.m
+++ src/LqrTrans.m
@@ -32,9 +32,9 @@ K = lqr(A,B,Q,R);
 
 % Construct the closed-loop system
 Ac = A - B*K;
-Bc = B;
+Bc = B*K;
 Cc = C;
-Dc = D;
+Dc = D*K;
 
 % Create state-space model of the closed-loop system
 sys_cl  = ss(Ac, Bc, Cc, Dc);
@@ -46,19 +46,61 @@ GCL = tf(sys_cl);
 % disp('Transfer Function of the Closed-Loop System with LQR Controller:');
 % sys_tf
 
-% display all bode plots on one plot
-%bode(sys_tf) 
+% % get the system transfer function matrix
+% load sysoltf.mat
+% GPLANT = zeros(6,3);
+% GPLANT = tf(GPLANT);
+% for i = 1:3
+%     for j = 1:6
+%         GPLANT(j,i) = H_ex{i,j};
+%     end
+% end
+% 
+% % open loop tf
+% GOL = GPLANT*GC; % state error -> state
+% 
+% % closed loop tf
+% GCL = GOL/(1+GOL);
 
-% individual bode plots
-for in = 1
-    for out = 1
-        % make plot
-        figure
-        bode(GCL(out,in))
-        grid on
-        fig_title = ['Bode plot for input = ' ]
-        
+
+% H1 = transpose(H);
+% T = (H1*GC)/(1+(H1*GC))
+
+% bode(T)
+
+
+%% individual bode plots
+inputname = ["x_{ref}","y_{ref}","z_{ref}","u_{ref}","v_{ref}","w_{ref}"];
+inputfilename = ["xref","yref","zref","uref","vref","wref"];
+outputname = ["x","y","z","u","v","w"];
+
+filename = @(in,out) join(["Plots/Bode Plots/Individual LQR Bode/","LQR_bode_",inputfilename(in),"_to_",outputname(out)],"");
+fig_title = @(in,out) join(['LQR Bode Plot: ',inputname(in),' \rightarrow ',outputname(out)],"");
+
+p = bodeoptions('cstprefs')
+p.grid = 'on';
+
+close_plots = true;
+% for every reference->state pair
+for in = 1:6
+    for out = 1:6
+        plotBode(in,out,GCL,fig_title(in,out),filename(in,out),p)
+        if close_plots
+            close gcf
+        end
     end
 end
 
+
+%% functions
+% plots and saves as png
+function [] = plotBode(in,out,tf,plot_title,filename,opt)
+    figure
+    freq = {1e-6, 1e4}; % periods between of 72 days to 0.06 seconds
+    bodeplot(tf(in,out),opt,freq)
+    title(plot_title)
+    print(filename,'-dpng')
+    % print(filename,'-dpdf','-fillpage')
+    %saveas(gcf, filename)
+end
 
